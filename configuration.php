de9d69c2df9fb617a1612057b5e9d3e03926daab
Gersón Gómez <ggomez@codemonkey.com.mx>
2021-05-24 14:37:29 -0500

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -229,7 +229,7 @@ class Configuration
 	 *
      * @var string $smtp_host
      */
-	public static $smtp_host = 'mail.livingtulum.com';
+	public static $smtp_host = 'mail.codemonkey.com.mx';
 
 	/**
      * Usuario de la cuenta smtp.
@@ -258,7 +258,7 @@ class Configuration
 	 *
      * @var string $smtp_secure
      */
-	public static $smtp_secure = 'tls';
+	public static $smtp_secure = 'ssl';
 
 	/**
      * Puerto de conexion al servidor SMTP.
@@ -267,7 +267,7 @@ class Configuration
 	 *
      * @var integer $smtp_port
      */
-	public static $smtp_port = 587;
+	public static $smtp_port = 465;
 
 	/**
      * Correo electrónico de emailing.
